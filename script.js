5c0a95954267ec578d0d630ef88e46a738316c5a
tirzasam07 <83017742+tirzasam07@users.noreply.github.com>
2024-02-08 14:20:25 +0100

    commit counter

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // document.getElementById("count").innerText = 0
-
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
-
-console.log(countEl);
 
 let count = 0
 
@@ -10,3 +8,14 @@ function increment() {
     count = count + 1
     countEl.innerText = count
 }
+
+
+function save() {
+    let countString = count + " - "
+    saveEl.textContent += countString
+    countEl.textContent = 0
+    // reset the count back to set 0 after save because JS remember the initial count. 
+    count = 0
+}
+
+save()
---
+++ string/string.js
@@ -0,0 +1,6 @@
+let myName = "Bob"
+let message = "Hi, My name is "
+
+let myGreeting = message + " " + myName
+
+console.log(myGreeting);
